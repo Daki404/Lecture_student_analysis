897c7d1a46967649f9adc3d1231b45978e205db8
Daki <daki403.1@gmail.com>
2021-08-23 20:05:53 +0900

    정규표현식, 게시글 링크 추출 성공

--- codingcamp user/test.py
+++ codingcamp user/test.py
@@ -5,12 +5,12 @@ import re
 html = urlopen('https://cafe.naver.com/ArticleList.nhn?search.clubid=30367563&search.menuid=75&search.boardtype=L')
 bs = BeautifulSoup(html.read(), 'html.parser', from_encoding='ANSI')
 
-tmp = bs.findAll(class_='article')
-p = re.compile('href=".*Articles=false"')
+tmp = bs.find_all(class_='article', href=re.compile('.*Articles=false'))
+for i in tmp:
+    print(i.get('href'))
 
-a = p.finditer(str(tmp))
-for i in a:
-    print(i)
+
+
 '''
 with open('cafe.txt', 'w', encoding='utf-8') as f:
     for i in tmp:
