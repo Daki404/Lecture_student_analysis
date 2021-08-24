b49cd1271b121b42fb708a1c0c0009f7dee7ddb8
Daki <daki403.1@gmail.com>
2021-08-24 19:17:38 +0900

    rename 및 파일정리\

--- codingcamp user/page_changer.py
+++ codingcamp user/page_changer.py
@@ -5,7 +5,7 @@ session = requests.Session()
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
 
 
-url = 'https://cafe.naver.com/ArticleList.nhn?search.clubid=30367563&search.menuid=75&search.boardtype=L&search.totalCount=151&search.cafeId=30367563&search.page=14'
+url = 'https://cafe.naver.com/ArticleList.nhn?search.clubid=30367563&search.menuid=75&search.boardtype=L&search.totalCount=151&search.cafeId=30367563&search.page=2'
 req = session.get(url, headers=headers)
 
 bs = BeautifulSoup(req.text, 'html.parser')
