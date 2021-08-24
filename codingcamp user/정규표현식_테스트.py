52ef3dd1152b67477635eb42db73d95bf35d337f
Daki <daki403.1@gmail.com>
2021-08-24 14:25:00 +0900

    url변환 페기, 확보환 query링크를 수정하여 데이터 접근예정

--- codingcamp user/정규표현식_테스트.py
+++ codingcamp user/정규표현식_테스트.py
@@ -1,9 +1,7 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
 import re
 
 p = re.compile('몰라')
 text = '안녕 니 이름은 뭐야? 몰라 멍청아'
 print(text)
-m = re.match('몰라', text)
-print(m)
+m = p.search(text)
+print(m.group())
