188ec254d618c604bdf8b48336cc756d5d42e99e
Daki <daki403.1@gmail.com>
2021-08-25 15:02:08 +0900

    학번 그래프화 성공

--- codingcamp user/data_maker.py
+++ codingcamp user/data_maker.py
@@ -1,9 +1,8 @@
 from post_num_parser import make_url
 import post_num_parser as post_parser
 import post_content_parser as content_parser
-import matplotlib.pyplot as plt
-import seaborn as sns
 import re
+
 
 user_name = []
 user_major = []
@@ -12,7 +11,19 @@ user_tag = []
 user_aim = []
 user_letter = []
 
-p = re.compile('[0-9]+')
+
+def id_data_mask(num):
+    p = re.compile('[0-9]+')
+    new = p.search(num).group().strip()
+    if len(new) == 2:
+        new = new
+    elif len(new) == 4 or len(new) == 8:
+        new = new[2:4]
+    else:
+        return False
+    if 10 <= int(new) <= 21:
+        return new
+    return False
 
 
 def parse_page(url=None):
@@ -29,40 +40,29 @@ def parse_page(url=None):
                 tmp = content_parser.soup_parser(soup)
                 user_name.append(tmp[0])
                 user_major.append(tmp[1])
-                new = p.search(tmp[2]).group()
-                if len(new) == 2:
-                    user_id.append(new)
-                elif len(new) == 4:
-                    user_id.append(new[-2:])
-                elif len(new) == 8:
-                    user_id.append(new[2:4])
-
-
+                if (st_id := id_data_mask(tmp[2])):
+                    user_id.append(int(st_id))
                 user_tag.append(tmp[3])
                 user_aim.append(tmp[4])
                 user_letter.append(tmp[5])
             except:
-                user_name.append('')
-                user_major.append('')
-                user_id.append('')
-                user_tag.append('')
-                user_aim.append('')
-                user_letter.append('')
+                pass
         return True
     else:
         return False
 
 
-parse_page()
-page_num = 2
+def data_parsing():
+    parse_page()
+    page_num = 2
 
-while True:
-    url = make_url(page_num)
-    if not parse_page(url):
-        break
-    page_num += 1
+    while True:
+        url = make_url(page_num)
+        if not parse_page(url):
+            break
+        page_num += 1
 
-ax = plt.subplots()
-ax = sns.countplot(user_id)
+    return user_name, user_id, user_major, user_tag, user_aim, user_letter
 
-plt.show()
+
+
